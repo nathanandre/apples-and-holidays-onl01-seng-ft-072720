570f346ec320f953161141c21876e1b2dd96062b
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 02:44:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,6 +31,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       puts "  #{holiday_name.to_s.split( pattern "_").collect {|split_holiday_name| split_holiday_name.capitalize}.join( separator " ") }: #{supplies.join(", ")}"
+    end
+  end
   
 
 end
