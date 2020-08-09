fce3519434af57dea3a39090f31112bbfbd8acbf
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 02:54:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,8 +38,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday_name, supplies|
+      supplies.include? ("BBQ") ? holiday_name : nil 
 
 end
 
