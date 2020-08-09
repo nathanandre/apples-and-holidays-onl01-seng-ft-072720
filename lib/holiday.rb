8892a127a66c4b4ee24195ad7b306698b6011b5e
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 02:27:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each |holiday_name, supplies|
+      puts "  "
   
 
 end
