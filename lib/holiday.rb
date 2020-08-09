e43cc9c7cbad0cf9f9bbb2803fb8b4bb3725a43d
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 02:56:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday_name, supplies|
-      supplies.include? ("BBQ") ? holiday_name : nil 
+      supplies.include?("BBQ") ? holiday_name : nil 
     end 
   end.flatten.compact
 
